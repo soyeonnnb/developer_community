bcf713d91c1c69af8c9b4342b41a296cef5fdb4f
soyeonnnb <la28s5d@naver.com>
2021-08-03 23:52:14 +0900

    add register

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path("login", user_views.login, name="login"),
     path("logout", user_views.logout, name="logout"),
+    path("sign_up", user_views.sign_up, name="sign_up"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -19,3 +20,15 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect("snsapp:home")
+
+
+def sign_up(request):
+    if request.method == "POST":
+        if request.POST["password"] == request.POST["repeat"]:
+            new_user = User.objects.create_user(
+                username=request.POST["username"], password=request.POST["password"]
+            )
+            auth.login(request, new_user)
+            return redirect("snsapp:home")
+
+    return render(request, "users/register.html")
